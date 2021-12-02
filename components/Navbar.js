c83e485f962fe83e10e3e614b95e1589f04edb23
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-12-02 12:55:06 +0000

    Added navigation headers to Navbar and basic styling. Installed FontAwesome for Icon usage.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,22 @@
 import style from "./Navbar.module.scss";
 
 const Navbar = () => {
-  return <nav className={style.navbar}>hello</nav>;
+  return (
+    <nav className={style.navbar}>
+      <div className={style.title}>Golden Shoe</div>
+      <div className={style.options}>
+        <div className={style.option}>Home</div>
+        <div className={style.option}>products</div>
+        <div className={style.option}>My Account</div>
+        <div className={style.option}>About</div>
+        <div className={style.option}>Contact</div>
+        <div className={style.cart}>
+          <div>Your cart</div>
+          <div>Shoes: 4 Price: £193.10</div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
