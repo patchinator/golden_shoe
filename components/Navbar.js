36db834d763f1fef488d91774f40cdc92a1ffbec
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-12-02 15:00:48 +0000

    Added assets folder for static images. Updated styling of Navbar component.

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,36 +8,40 @@ import {
   faBookOpen,
   faShoppingCart,
 } from "@fortawesome/free-solid-svg-icons";
+import logo from "../assets/golden_shoe_logo.png";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <nav className={style.navbar}>
-      <div className={style.title}>Golden Shoe</div>
+      <div className={style.title}>
+        <Image src={logo} width="140px" height="50px" alt="Golden Shoe Logo" />
+      </div>
       <div className={style.options}>
         <div className={style.option}>
-          <FontAwesomeIcon width="1.25rem" icon={faHome} />
           <p>Home</p>
+          <FontAwesomeIcon width="1.25rem" icon={faHome} />
         </div>
         <div className={style.option}>
-          <FontAwesomeIcon width="1.25rem" icon={faShoePrints} />
           <p>Products</p>
+          <FontAwesomeIcon width="1.25rem" icon={faShoePrints} />
         </div>
         <div className={style.option}>
-          <FontAwesomeIcon width="1.25rem" icon={faUser} />
           <p>My Account</p>
+          <FontAwesomeIcon width="1.25rem" icon={faUser} />
         </div>
         <div className={style.option}>
-          <FontAwesomeIcon width="1.25rem" icon={faBookOpen} />
           <p>About</p>
+          <FontAwesomeIcon width="1.25rem" icon={faBookOpen} />
         </div>
         <div className={style.option}>
-          <FontAwesomeIcon width="1.25rem" icon={faPhone} />
           <p>Contact</p>
+          <FontAwesomeIcon width="1.25rem" icon={faPhone} />
         </div>
         <div className={style.cart}>
           <div className={style.cart_text}>
             <FontAwesomeIcon width="1.25rem" icon={faShoppingCart} />
-            <p>Your shopping cart</p>
+            <p>Your shopping cart:</p>
           </div>
           <div>Shoes: 4 Price: £193.10</div>
         </div>
