2d7e82323f107e27edd2a3150ad9930bac37f5b5
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-12-03 12:15:29 +0000

    Added media.scss file to contain all media queries across app. Updated Navbar to consist of a custom dropdown.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import style from "./Navbar.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -7,45 +9,60 @@ import {
   faShoePrints,
   faBookOpen,
   faShoppingCart,
+  faBars,
+  faArrowDown,
 } from "@fortawesome/free-solid-svg-icons";
 import logo from "../assets/golden_shoe_logo.png";
 import Image from "next/image";
 
 const Navbar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const dropdownHandler = () => {
+    setShowDropdown((prev) => !prev);
+  };
+
   return (
     <nav className={style.navbar}>
       <div className={style.title}>
         <Image src={logo} width="140px" height="50px" alt="Golden Shoe Logo" />
       </div>
-      <div className={style.options}>
-        <div className={style.option}>
-          <p>Home</p>
-          <FontAwesomeIcon width="1.25rem" icon={faHome} />
-        </div>
-        <div className={style.option}>
-          <p>Products</p>
-          <FontAwesomeIcon width="1.25rem" icon={faShoePrints} />
-        </div>
-        <div className={style.option}>
-          <p>My Account</p>
-          <FontAwesomeIcon width="1.25rem" icon={faUser} />
-        </div>
-        <div className={style.option}>
-          <p>About</p>
-          <FontAwesomeIcon width="1.25rem" icon={faBookOpen} />
-        </div>
-        <div className={style.option}>
-          <p>Contact</p>
-          <FontAwesomeIcon width="1.25rem" icon={faPhone} />
-        </div>
-        <div className={style.cart}>
-          <div className={style.cart_text}>
-            <FontAwesomeIcon width="1.25rem" icon={faShoppingCart} />
-            <p>Your shopping cart:</p>
-          </div>
-          <div>Shoes: 4 Price: £193.10</div>
-        </div>
+
+      <div className={style.dropdown_button} onClick={dropdownHandler}>
+        <FontAwesomeIcon className={style.icon} icon={faBars} />
       </div>
+
+      {showDropdown && (
+        <div className={style.dropdown}>
+          <div className={style.option}>
+            <p>Home</p>
+            <FontAwesomeIcon className={style.icon} icon={faHome} />
+          </div>
+          <div className={style.option}>
+            <p>Products</p>
+            <FontAwesomeIcon className={style.icon} icon={faShoePrints} />
+          </div>
+          <div className={style.option}>
+            <p>My Account</p>
+            <FontAwesomeIcon className={style.icon} icon={faUser} />
+          </div>
+          <div className={style.option}>
+            <p>About</p>
+            <FontAwesomeIcon className={style.icon} icon={faBookOpen} />
+          </div>
+          <div className={style.option}>
+            <p>Contact</p>
+            <FontAwesomeIcon className={style.icon} icon={faPhone} />
+          </div>
+        </div>
+      )}
+      {/* <div className={style.cart}>
+        <div className={style.cart_text}>
+          <FontAwesomeIcon className={style.icon} icon={faShoppingCart} />
+          <p>Your shopping cart:</p>
+        </div>
+        <div>Shoes: 4 Price: £193.10</div>
+      </div> */}
     </nav>
   );
 };
