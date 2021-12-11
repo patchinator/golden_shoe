3fb06e872f6937129db62d15f82577bdb4750b15
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-12-11 18:53:18 +0000

    Added relevant components for contact

---
+++ components/ContactCard.js
@@ -0,0 +1,7 @@
+import style from "./ContactCard.module.scss";
+
+const ContactCard = () => {
+  return <div>contact card</div>;
+};
+
+export default ContactCard;
--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,14 @@
+import ContactCard from "../components/ContactCard";
 import Navbar from "../components/Navbar";
 
 const ContactPage = () => {
   return (
     <section>
       <Navbar />
-      <div>Contacts Page</div>
+      <section>
+        <div>Contact Us</div>
+        <ContactCard />
+      </section>
     </section>
   );
 };
