f75cd772c7d1e833bfb7bd11fee4c187954c77c0
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-12-11 20:34:59 +0000

    added FAQ card info via props on ContactCards component

--- components/Contact/ContactCard.js
+++ components/Contact/ContactCard.js
@@ -1,14 +1,14 @@
 import style from "./ContactCard.module.scss";
 
-const ContactCard = () => {
+const ContactCard = (props) => {
   return (
     <div className={style.card}>
-      <div className={style.card_header}>top</div>
+      <div className={style.card_header}>{props.header}</div>
       <div className={style.card_links}>
-        <div>helpoo</div>
-        <div>helo</div>
-        <div>heloop</div>
-        <div>helplay</div>
+        <div>{props.link_one}</div>
+        <div>{props.link_two}</div>
+        <div>{props.link_three}</div>
+        <div className={style.expand}>View All</div>
       </div>
     </div>
   );
