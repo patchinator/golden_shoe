2bdb86c0e49248eba2fd277f2d990440567b459f
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-12-11 22:11:06 +0000

    Added relevant headers for Footer menu

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,10 +5,32 @@ const Footer = () => {
     <footer className={style.footer}>
       <div className={style.social_media}>socials</div>
       <div className={style.column_wrapper}>
-        <div className={style.columns}>column 1</div>
-        <div className={style.columns}>column 2</div>
-        <div className={style.columns}>column 3</div>
-        <div className={style.columns}>column 4</div>
+        <div className={style.columns}>
+          <div>Help & Info</div>
+          <div>Help</div>
+          <div>Track Order</div>
+          <div>Deliver & Returns</div>
+          <div>Student Discount</div>
+        </div>
+        <div className={style.columns}>
+          <div>About Golden Shoe</div>
+          <div>About us</div>
+          <div>Careers at Golden Shoe</div>
+          <div>Cyber Security</div>
+        </div>
+        <div className={style.columns}>
+          <div>More Info</div>
+          <div>Black Friday</div>
+          <div>Weekly Deals</div>
+          <div>Weekly Deals</div>
+          <div>Gift Vouchers</div>
+        </div>
+        <div className={style.columns}>
+          <div>T&C</div>
+          <div>Privacy and Cookies</div>
+          <div>Terms and Conditions</div>
+          <div>Accessibility</div>
+        </div>
       </div>
     </footer>
   );
