6bbfc832612e22a705320a711c2ff97b4e7f6147
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-12-12 13:38:50 +0000

    finished design on second desktop image component

--- components/DesktopImageTwo.js
+++ components/DesktopImageTwo.js
@@ -6,13 +6,13 @@ const DesktopImageTwo = (props) => {
     <div className={style.image}>
       <Image src={props.image} alt={props.imageAlt} />
       <div className={style.image_text_card}>
-        <div>Winter sales have arrived!</div>
-        <div>20% off until Jan 2nd</div>
+        <div>Wrap up warm this December</div>
+        <div>mens boots from £49.99</div>
       </div>
       <div className={style.image_text_disclaimer}>
         <div>
-          20% off only applies when £50 or more is spent online. Terms and
-          Conditions apply. For more information check out T&C on our website.
+          Click to check out what we have in stock. Please note that delivery
+          times may be slower due to high demand.
         </div>
       </div>
     </div>
