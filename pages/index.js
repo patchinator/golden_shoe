c9e7845bfdf821c87965bbebc4b109b3fdd741ac
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-12-12 12:17:53 +0000

    added wrapping background component for styling on bigger devices

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Navbar from "../components/Header/Navbar";
 import ProductCard from "../components/ProductCard";
 import Carousel from "../components/Carousel";
 import Footer from "../components/Footer/Footer";
+import Background from "../components/UI/Background";
 
 // card images
 import womensShoesImage from "../assets/womens_shoes.jpg";
@@ -25,36 +26,38 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <Carousel
-        imageOne={carouselImageOne}
-        imageOneAlt="Doc Martens"
-        imageOneDesc="Save 30% online"
-        imageTwo={carouselImageTwo}
-        imageTwoAlt="Womens Shoes"
-        imageTwoDesc="Treat yourself"
-        imageThree={carouselImageThree}
-        imageThreeAlt="Fancy mens Shoes"
-        imageThreeDesc="Suit the occassion"
-      />
-      <ProductCard
-        title="Womens Shoes"
-        text="Fantastic current trends at our lowest prices. Click to find out more."
-        image={womensShoesImage}
-        price="64.99"
-      />
-      <ProductCard
-        title="Running Shoes"
-        text="Burn off that lockdown waistline in style! We have all the top brands stocked."
-        image={trainersImage}
-        price="42.50"
-      />
-      <ProductCard
-        title="Womens Boots"
-        text="Winter is here. Keep those feet snuggly with some of the finest and warmest
+      <Background>
+        <Carousel
+          imageOne={carouselImageOne}
+          imageOneAlt="Doc Martens"
+          imageOneDesc="Save 30% online"
+          imageTwo={carouselImageTwo}
+          imageTwoAlt="Womens Shoes"
+          imageTwoDesc="Treat yourself"
+          imageThree={carouselImageThree}
+          imageThreeAlt="Fancy mens Shoes"
+          imageThreeDesc="Suit the occassion"
+        />
+        <ProductCard
+          title="Womens Shoes"
+          text="Fantastic current trends at our lowest prices. Click to find out more."
+          image={womensShoesImage}
+          price="64.99"
+        />
+        <ProductCard
+          title="Running Shoes"
+          text="Burn off that lockdown waistline in style! We have all the top brands stocked."
+          image={trainersImage}
+          price="42.50"
+        />
+        <ProductCard
+          title="Womens Boots"
+          text="Winter is here. Keep those feet snuggly with some of the finest and warmest
          boots you've ever tried."
-        image={womensBootsImage}
-        price="59.99"
-      />
+          image={womensBootsImage}
+          price="59.99"
+        />
+      </Background>
       <Footer />
     </div>
   );
