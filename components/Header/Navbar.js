26dee85bd5cce6fb3dc8a16773d3e07b8fcf8735
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-12-12 10:11:59 +0000

    added new color for secondary navbar level to display additional options on larger displays

--- components/Header/Navbar.js
+++ components/Header/Navbar.js
@@ -18,6 +18,8 @@ import {
   faBars,
   faArrowDown,
   faSearch,
+  faShoppingBag,
+  faHeart,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
@@ -48,76 +50,98 @@ const Navbar = () => {
 
   return (
     <nav ref={dropdownRef} className={style.navbar}>
-      <div className={style.logo}>
-        <Link href="/" passHref>
-          <a>
-            <Image
-              src={logo}
-              width="140px"
-              height="50px"
-              alt="Golden Shoe Logo"
-            />
-          </a>
-        </Link>
-      </div>
-
-      <div className={style.men}>
-        <p>Men</p>
-      </div>
-      <div className={style.women}>
-        <p>Women</p>
-      </div>
-      <div className={style.kids}>
-        <p>Kids</p>
-      </div>
-      <div className={style.search}>
-        <form>
-          <input type="text" />
-          <button>
-            <FontAwesomeIcon width="1rem" icon={faSearch} />
-          </button>
-        </form>
-      </div>
-
-      <div className={style.dropdown_button} onClick={dropdownHandler}>
-        {!showDropdown && (
-          <FontAwesomeIcon className={style.icon} icon={faBars} />
-        )}
-        {showDropdown && (
-          <FontAwesomeIcon className={style.icon} icon={faArrowDown} />
-        )}
-      </div>
-
-      {showDropdown && (
-        <div className={style.dropdown}>
+      <div className={style.navbar_top}>
+        <div className={style.logo}>
           <Link href="/" passHref>
-            <div className={style.option}>
-              <p>Home</p>
-              <FontAwesomeIcon className={style.icon} icon={faHome} />
-            </div>
-          </Link>
-          <Link href="/products" passHref>
-            <div className={style.option}>
-              <p>Products</p>
-              <FontAwesomeIcon className={style.icon} icon={faShoePrints} />
-            </div>
-          </Link>
-          <div className={style.option}>
-            <p>My Account</p>
-            <FontAwesomeIcon className={style.icon} icon={faUser} />
-          </div>
-          <div className={style.option}>
-            <p>About</p>
-            <FontAwesomeIcon className={style.icon} icon={faBookOpen} />
-          </div>
-          <Link href="/contact" passHref>
-            <div className={style.option}>
-              <p>Contact</p>
-              <FontAwesomeIcon className={style.icon} icon={faPhone} />
-            </div>
+            <a>
+              <Image
+                src={logo}
+                width="140px"
+                height="50px"
+                alt="Golden Shoe Logo"
+              />
+            </a>
           </Link>
         </div>
-      )}
+
+        <div className={style.men}>
+          <p>Men</p>
+        </div>
+        <div className={style.women}>
+          <p>Women</p>
+        </div>
+        <div className={style.kids}>
+          <p>Kids</p>
+        </div>
+
+        <div className={style.search}>
+          <form>
+            <input type="text" />
+            <button>
+              <FontAwesomeIcon width="1rem" icon={faSearch} />
+            </button>
+          </form>
+        </div>
+
+        <div className={style.desktop_icons}>
+          <div>
+            <FontAwesomeIcon className={style.icon} icon={faHome} />
+          </div>
+          <div>
+            <FontAwesomeIcon className={style.icon_user} icon={faUser} />
+          </div>
+          <div>
+            <FontAwesomeIcon className={style.icon_heart} icon={faHeart} />
+          </div>
+          <div>
+            <FontAwesomeIcon className={style.icon_bag} icon={faShoppingBag} />
+          </div>
+        </div>
+
+        <div className={style.dropdown_button} onClick={dropdownHandler}>
+          {!showDropdown && (
+            <FontAwesomeIcon className={style.icon} icon={faBars} />
+          )}
+          {showDropdown && (
+            <FontAwesomeIcon className={style.icon} icon={faArrowDown} />
+          )}
+        </div>
+
+        {showDropdown && (
+          <div className={style.dropdown}>
+            <Link href="/" passHref>
+              <div className={style.option}>
+                <p>Home</p>
+                <FontAwesomeIcon className={style.icon} icon={faHome} />
+              </div>
+            </Link>
+            <Link href="/products" passHref>
+              <div className={style.option}>
+                <p>Products</p>
+                <FontAwesomeIcon className={style.icon} icon={faShoePrints} />
+              </div>
+            </Link>
+            <div className={style.option}>
+              <p>My Account</p>
+              <FontAwesomeIcon className={style.icon} icon={faUser} />
+            </div>
+            <div className={style.option}>
+              <p>About</p>
+              <FontAwesomeIcon className={style.icon} icon={faBookOpen} />
+            </div>
+            <Link href="/contact" passHref>
+              <div className={style.option}>
+                <p>Contact</p>
+                <FontAwesomeIcon className={style.icon} icon={faPhone} />
+              </div>
+            </Link>
+          </div>
+        )}
+      </div>
+
+      <div className={style.navbar_bottom}>
+        <div>test</div>
+      </div>
     </nav>
   );
 };
