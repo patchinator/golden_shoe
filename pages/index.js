7e20dfaad02dd3407dc60c3f862e3490cfe73453
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-12-12 12:44:30 +0000

    added image for desktop home screen, updated current components to render only on tablet and mobile

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,12 @@ import Navbar from "../components/Header/Navbar";
 import ProductCard from "../components/ProductCard";
 import Carousel from "../components/Carousel";
 import Footer from "../components/Footer/Footer";
-import Background from "../components/UI/Background";
 
 // card images
 import womensShoesImage from "../assets/womens_shoes.jpg";
 import trainersImage from "../assets/trainers.jpg";
 import womensBootsImage from "../assets/womens_boots.jpg";
+import desktopBooksImage from "../assets/desktop_image_one.jpg";
 
 // carousel images
 import carouselImageOne from "../assets/womens_boots_2.jpg";
@@ -26,38 +26,36 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <Background>
-        <Carousel
-          imageOne={carouselImageOne}
-          imageOneAlt="Doc Martens"
-          imageOneDesc="Save 30% online"
-          imageTwo={carouselImageTwo}
-          imageTwoAlt="Womens Shoes"
-          imageTwoDesc="Treat yourself"
-          imageThree={carouselImageThree}
-          imageThreeAlt="Fancy mens Shoes"
-          imageThreeDesc="Suit the occassion"
-        />
-        <ProductCard
-          title="Womens Shoes"
-          text="Fantastic current trends at our lowest prices. Click to find out more."
-          image={womensShoesImage}
-          price="64.99"
-        />
-        <ProductCard
-          title="Running Shoes"
-          text="Burn off that lockdown waistline in style! We have all the top brands stocked."
-          image={trainersImage}
-          price="42.50"
-        />
-        <ProductCard
-          title="Womens Boots"
-          text="Winter is here. Keep those feet snuggly with some of the finest and warmest
+      <Carousel
+        imageOne={carouselImageOne}
+        imageOneAlt="Doc Martens"
+        imageOneDesc="Save 30% online"
+        imageTwo={carouselImageTwo}
+        imageTwoAlt="Womens Shoes"
+        imageTwoDesc="Treat yourself"
+        imageThree={carouselImageThree}
+        imageThreeAlt="Fancy mens Shoes"
+        imageThreeDesc="Suit the occassion"
+      />
+      <ProductCard
+        title="Womens Shoes"
+        text="Fantastic current trends at our lowest prices. Click to find out more."
+        image={womensShoesImage}
+        price="64.99"
+      />
+      <ProductCard
+        title="Running Shoes"
+        text="Burn off that lockdown waistline in style! We have all the top brands stocked."
+        image={trainersImage}
+        price="42.50"
+      />
+      <ProductCard
+        title="Womens Boots"
+        text="Winter is here. Keep those feet snuggly with some of the finest and warmest
          boots you've ever tried."
-          image={womensBootsImage}
-          price="59.99"
-        />
-      </Background>
+        image={womensBootsImage}
+        price="59.99"
+      />
       <Footer />
     </div>
   );
